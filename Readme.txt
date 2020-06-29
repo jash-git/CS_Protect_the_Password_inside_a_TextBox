5a5d60ab7c4fd78a43587ef1369595b23064e9c1
Gitea <gitea@fake.local>
2020-06-29 08:45:40 +0800

    Create

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+C# 防止密碼偵測(竊取/偷窺/透視)工具[spy++]的TextBox元件『myPassBox』(CS_Protect_the_Password_inside_a_TextBox)
+
+
+資料來源:https://dotblogs.com.tw/ricochen/2009/12/16/12485
+https://codingvision.net/security/c-protect-the-password-inside-a-textbox
+
+微軟工具:C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\spyxx.exe
+外部工具:xkvp_4.5.20090205_azo
---
+++ code/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myPassBox1.Text = "1233456789";
+            textBox1.Text = "1233456789";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(myPassBox1.Text);
+        }
+    }
+}
